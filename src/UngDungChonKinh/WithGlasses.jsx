9f9a360f5df75e72d7d36c33a926d47ed8e49388
Nghia-Truong <chuon.chongia1306@gmail.com>
2024-10-06 23:37:43 +0900

    reset code 2nd time

--- src/UngDungChonKinh/WithGlasses.jsx
+++ src/UngDungChonKinh/WithGlasses.jsx
@@ -8,7 +8,7 @@ export default function WithGlasses() {
             <img id="glassesImage" src={modelImgURL} alt="With Glasses" />
             <div ><img src="${pickedItem.url}" alt="" className={style.glasses_position} id='pickedGlasses' /></div>
             <div id="glassesDescription" className={style.description}>
-                <p className="fw-bold" id="glassesName"></p>
+                <p className="fw-bold" id="glassesName">With Glasses</p>
                 <p id="glassesDetail"></p>
             </div>
         </div >
